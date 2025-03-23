98455d41601edef76f8666b5318627308950204b
Diego Silva de Salles <disalles222@hotmail.com>
2025-03-22 21:48:11 -0300

    push cla first

---
+++ R/variance_threshold.R
@@ -0,0 +1,30 @@
+#'@title Feature Selection Using VarianceThreshold
+#'@description This module applies feature selection based on variance using VarianceThreshold.
+#'@import reticulate
+
+#'@param threshold The variance threshold below which features will be removed
+#'@return A Python object
+#'@export
+create_variance_threshold_model <- function(threshold=0.2) {
+  reticulate::source_python("inst/python/sklearn/feature_select/variance_threshold.py")
+  model <- fs_create(threshold=threshold)
+  return(model)
+}
+
+#'@param model The model
+#'@param df_train Data frame to transform
+#'@param target_column Target column name
+#'@return Transformed features
+#'@export
+fit_transform <- function(model, df_train, target_column) {
+  cat("Column types:", sapply(df_train, class), "\n")
+  if (!exists("fit_transform")) {
+    reticulate::source_python("inst/python/sklearn/feature_select/variance_threshold.py")
+  }
+
+  # Convert df_train to a pandas DataFrame
+  df_train_py <- reticulate::r_to_py(df_train)
+  X <- df_train_py$drop(target_column, axis=1)
+  result <- model$fit_transform(X)
+  return(reticulate::py_to_r(result))
+}
