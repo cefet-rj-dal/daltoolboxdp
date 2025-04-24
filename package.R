01cdef67344530faaaaa165108b1c08b6091fb54
eogasawara@ieee.org <eogasawara@ieee.org>
2025-04-24 01:19:50 -0300

    update

--- package.R
+++ package.R
@@ -37,6 +37,8 @@ if (FALSE) {
 
 if (FALSE) {
   #update documentation
+  devtools::document()
+  devtools::check()
   pkgdown::build_site()
 }
 if (FALSE) {
@@ -54,10 +56,10 @@ if (FALSE) { #build package for cran
   #run in RStudio
   library(devtools)
   pkgbuild::build(manual = TRUE)
-
+  
   #run in terminal
-  #R CMD check daltoolboxdp_1.0.777.tar.gz
-  #R CMD check daltoolboxdp_1.0.777.tar.gz --as-cran
+  #R CMD check daltoolboxdp_1.0.787.tar.gz
+  #R CMD check daltoolboxdp_1.0.787.tar.gz --as-cran
 
   #upload package
   #https://cran.r-project.org/submit.html
